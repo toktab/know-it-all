c049d17e85060e3483bb8d2777219f0d4d05114c
toktab <toktabbusiness@gmail.com>
2026-03-06 18:01:10 +0400

    update: commented jackson line, because it produced some errors. add: application.properties, KnowItAllApplication.java (main runner file), env.requirements file and those properties to intellijidea environment variables each by each with correct values.

---
+++ src/main/java/com/knowitall/KnowItAllApplication.java
@@ -0,0 +1,11 @@
+package com.knowitall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowItAllApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KnowItAllApplication.class, args);
+    }
+}
